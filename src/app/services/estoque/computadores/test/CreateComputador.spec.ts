d50048bfe31aa19dc1002c55e5f4eed0398679fa
Guilherme Sousa <guilhermenoobsaibot@gmail.com>
2020-07-02 17:46:43 -0300

    init test unit

---
+++ src/app/services/estoque/computadores/test/CreateComputador.spec.ts
@@ -0,0 +1,9 @@
+import createComputador from "@app/services/estoque/computadores/CreateComputador"
+
+// Isso ou isto
+
+describe("CreateComputador", () => {
+    it("should be able to create a new computer", () => {
+        expect(1 + 2).toBe(3)
+    })
+})

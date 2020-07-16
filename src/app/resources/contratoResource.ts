29e169cb887d33b070acf538079a286f1ae88b9a
iDev1ce <riquelme.fab@gmail.com>
2020-07-16 16:12:42 -0300

    get contratos

--- src/app/resources/contratoResource.ts
+++ src/app/resources/contratoResource.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express"
+import { getCustomRepository } from "typeorm"
+import ContratoRepository from "@app/repositories/ContratoRepository"
 
 class ContratoResource {
     public async insert(req: Request, res: Response) {
@@ -6,7 +8,27 @@ class ContratoResource {
     }
 
     public async getAll(req: Request, res: Response) {
+        const contratoRepository = getCustomRepository(ContratoRepository)
+
+        const contratos = await contratoRepository.find()
+
+        if (!contratos)
+            return res.status(404).send({ message: "Não há contratos" })
+
+        return res.status(200).send(contratos)
+    }
+
+    public async getById(req: Request, res: Response) {
+        const contratoRepository = getCustomRepository(ContratoRepository)
+
+        const { id } = req.params
+
+        const contrato = await contratoRepository.findOne({ id })
+
+        if (!contrato)
+            return res.status(404).send({ message: "Não existe contrato" })
         
+        return res.status(200).send(contrato)
     }
 }
 
